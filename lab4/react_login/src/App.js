3e875f048b05ba299df886d5515e71598ba594db
Doreen0218 <162420787+Doreen0218@users.noreply.github.com>
2024-05-08 15:00:15 +0800

    Add files via upload

---
+++ lab4/react_login/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import SignInSide from './SignInSide'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <SignInSide />
+      </div>
+    </div>
+  );
+}
+
+export default App;
